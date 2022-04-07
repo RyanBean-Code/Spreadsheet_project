586547bab46bf61f2d98f4d51ee85e9c8d9eb683
rbean <ryan.bean@wsu.edu>
2022-04-06 20:09:21 -0700

    PerformUndoRedo method now added to the UndoRedoCollection class

--- SpreadSheetProject/Spreadsheet_Ryan_Bean/SpreadsheetEngine/UndoRedoCollection.cs
+++ SpreadSheetProject/Spreadsheet_Ryan_Bean/SpreadsheetEngine/UndoRedoCollection.cs
@@ -6,7 +6,9 @@ namespace SpreadsheetEngine
 {
     using System;
     using System.Collections.Generic;
+    using System.Diagnostics;
     using System.Linq;
+    using System.Reflection;
     using System.Text;
     using System.Threading.Tasks;
 
@@ -15,17 +17,47 @@ namespace SpreadsheetEngine
     /// </summary>
     internal class UndoRedoCollection
     {
+        /// <summary>
+        /// Represents the method name needed to make a undo or redo action.
+        /// </summary>
         private string methodName;
 
+        /// <summary>
+        /// Represents the owner of the method name.
+        /// </summary>
         private object owner;
 
+        /// <summary>
+        /// Represents the list of parameters in the method call.
+        /// </summary>
         private List<object> parameters;
 
-        public UndoRedoCollection(string newMethodName, object newOwner, List<object> newParameters)
+        /// <summary>
+        /// Initializes a new instance of the <see cref="UndoRedoCollection"/> class.
+        /// </summary>
+        /// <param name="newMethodName"> The name of the method. </param>
+        /// <param name="newOwner"> The owner of the method. </param>
+        /// <param name="newParameters"> The parameters for the method. </param>
+        public UndoRedoCollection(string newMethodName, object newOwner, List<object> newParameters, string? newUndoRedoName = null)
         {
             this.methodName = newMethodName;
             this.owner = newOwner;
             this.parameters = newParameters;
+            this.UndoRedoName = newUndoRedoName;
+        }
+
+        /// <summary>
+        /// Gets or sets the undoRedoName
+        /// </summary>
+        public string? UndoRedoName { get; set; };
+
+        /// <summary>
+        /// Calls the method which will either undo or redo the action.
+        /// </summary>
+        public void PerformUndoRedo()
+        {
+            MethodInfo method = this.owner.GetType().GetMethod(this.methodName, BindingFlags.NonPublic | BindingFlags.Static | BindingFlags.Instance);
+            method.Invoke(this.owner, new object[] { this.parameters });
         }
     }
 }
